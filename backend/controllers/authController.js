d90e5f47f6af5aa210596db7eec6371d084d1819
Tanushree-Pal-06 <tanushreepal2005@gmail.com>
2026-09-20 15:48:42 +0530

    Add password length validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,6 +16,11 @@ exports.registerUser = async (req, res) => {
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
+        if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters long",
+      });
+    }
 
     const existingUser = await User.findOne({ email });
 
